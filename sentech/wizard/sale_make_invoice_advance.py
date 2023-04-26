1e69be4b9882761749d2a87486a7130b04960567
GandG-OLN <olivier.nadieline@gandgcorp.com>
2023-04-26 12:28:06 +0000

    Update sale_make_invoice_advance.py

--- sentech/wizard/sale_make_invoice_advance.py
+++ sentech/wizard/sale_make_invoice_advance.py
@@ -6,10 +6,10 @@ _logger = logging.getLogger(__name__)
 
 class SaleAdvancePaymentInv(models.TransientModel):
     _inherit = 'sale.advance.payment.inv'
-
-    def _prepare_invoice_values(self, order, name, amount, so_line):
+    
+    def _create_invoice(self, order, so_line, amount):
         _logger.debug("=============================== I N S I D E    F U N C T I O N =====================")
-        res = super()._prepare_invoice_values(order, name, amount, so_line)
-        res['libelle'] = order.libelle
-        return res
+        invoice = super()._create_invoice(self, order, so_line, amount)
+        invoice['libelle'] = order.libelle
+        return invoice
 
