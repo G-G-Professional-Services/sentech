838c02d0a124c9270c99b27e6c52c667b746b979
GandG-OLN <olivier.nadieline@gandgcorp.com>
2023-04-26 13:20:13 +0000

    Update sale_order.py

--- sentech/models/sale_order.py
+++ sentech/models/sale_order.py
@@ -8,15 +8,10 @@ class SaleOrder(models.Model):
 
     libelle = fields.Char(string="Libellé: ")
 
-    def action_view_invoice(self):
+    def _prepare_invoice(self):
         _logger.debug("=============================== I N S I D E    F U N C T I O N =====================")
-        action = super().action_view_invoice()
-        context = {
-            'default_move_type': 'out_invoice',
-        }
-        if len(self) == 1:
-            context.update({
-                'default_libelle': self.libelle,
-            })
-        action['context'] = context
-        return action
+        res = super()._prepare_invoice()
+        res['libelle'] = self.libelle
+        return res
+
+
